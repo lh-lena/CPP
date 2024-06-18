6b0230651534077224c52d0662937c73b9aa90d0
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-06-18 17:52:19 +0200

    chore: add more test

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -10,7 +10,8 @@ int	main()
 		a.increaseGrade();
 		std::cout << a << std::endl;
 	}
-	catch (const std::exception & e) {
+	catch (const std::exception & e)
+	{
 		std::cerr << e.what() << std::endl;
 	}
 	try
@@ -20,16 +21,35 @@ int	main()
 		a.decreaseGrade();
 		std::cout << a << std::endl;
 	}
-	catch (const std::exception & e) {
+	catch (const std::exception & e)
+	{
 		std::cerr << e.what() << std::endl;
 	}
+	try
+	{
+		Bureaucrat mr_id("MR_ID", 101);
+		Form id_form("380 FORM", 100, 90);
+		std::cout << id_form << std::endl;
 
+		mr_id.signForm(id_form);
+		std::cout << id_form << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat mr_id("MR_ID", 0);
+		Form id_form("380 FORM", 100, 90);
+		std::cout << id_form << std::endl;
 
-	Form id_form("380 FORM", 100, 90);
-	Bureaucrat mr_id("MR_ID", 101);
-	std::cout << id_form << std::endl;
-
-	mr_id.signForm(id_form);
-	std::cout << id_form << std::endl;
+		mr_id.signForm(id_form);
+		std::cout << id_form << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 	return (0);
 }
