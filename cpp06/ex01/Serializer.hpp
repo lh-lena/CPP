512cc97811cc59de23f9eba21f980df176d8f4f7
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-06-23 12:34:12 +0200

    chore: change library

--- cpp06/ex01/Serializer.hpp
+++ cpp06/ex01/Serializer.hpp
@@ -6,35 +6,28 @@
 /*   By: ohladkov <ohladkov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 11:52:51 by ohladkov          #+#    #+#             */
-/*   Updated: 2024/06/23 11:52:51 by ohladkov         ###   ########.fr       */
+/*   Updated: 2024/06/23 12:32:36 by ohladkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERIALIZER_HPP
 # define SERIALIZER_HPP
 
-# include <cstdint>
+# include <stdint.h>
 # include <iostream>
-# include "Data.h"
+# include "Data.hpp"
 
 class Serializer
 {
 public:
     static uintptr_t serialize(Data* ptr);
     static Data* deserialize(uintptr_t raw);
+
 private:
-    Serializer();
-    Serializer(const Serializer& src);
-    Serializer& operator=(const Serializer& src);
-    ~Serializer();
+    // Prevent instantiation by making constructors private
+    Serializer( void );
+    Serializer(const Serializer&);
+    Serializer& operator=(const Serializer&);
 };
-
-Serializer::Serializer()
-{
-}
-
-Serializer::~Serializer()
-{
-}
 
 #endif // SERIALIZER_H
