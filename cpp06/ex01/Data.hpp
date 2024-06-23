e2f19eef08b04c56e1ddf1ed91d6317a4eea8cd1
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-06-23 11:57:17 +0200

    feat: Implement a struct Data

---
+++ cpp06/ex01/Data.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Data.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ohladkov <ohladkov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/23 11:56:23 by ohladkov          #+#    #+#             */
+/*   Updated: 2024/06/23 11:56:24 by ohladkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DATA_HPP
+# define DATA_HPP
+
+#include <string>
+
+struct Data
+{
+    int intValue;
+    float floatValue;
+    std::string strValue;
+};
+
+
+#endif // DATA_HPP
