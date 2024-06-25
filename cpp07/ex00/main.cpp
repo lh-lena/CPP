6c1ceb21f090acc28f5318ebe118608ad1d9da25
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-06-25 18:17:54 +0200

    cpp07ex00: OK

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,30 @@
+
+#include "whatever.hpp"
+#include <iostream>
+
+int main( void )
+{
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
+}
+
+/**
+ * Using ::swap(a, b); in template functions
+ *  is a way to manage function lookup explicitly, 
+ * ensuring that the intended swap function is called, 
+ * avoiding potential issues with ADL, 
+ * and providing a clear and unambiguous 
+ * resolution path for the swap function.
+ * 
+*/
