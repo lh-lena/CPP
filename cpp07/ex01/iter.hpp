da765354c7958f3f2f942c92e268e1fbdda187db
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-06-26 22:32:04 +0200

    feat: cpp07/ex01: Implemented a function template iter

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,6 +1,20 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
-template< typename T > void iter(T a[], const T& length, );
+#include <iostream>
+
+template<typename T>void printElement(T element)
+{
+    std::cout << element << std::endl;
+} 
+
+template< typename T > void iter(T *arr, size_t length, void (*fun)(T))
+{
+    for (size_t i = 0; i < length; i++)
+    {
+        fun(arr[i]);
+    }
+}
+
 
 #endif
