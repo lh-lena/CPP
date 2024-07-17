c94e27ec388e48a025e5f2b8403c040edddec02c
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-07-17 12:07:11 +0200

    cpp09ex02: initiate class PmergeMe

---
+++ cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,16 @@
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+class PmergeMe
+{
+
+public:
+    PmergeMe();
+    PmergeMe(const PmergeMe& other);
+    PmergeMe& operator=(const PmergeMe& other);
+    ~PmergeMe();
+
+private:
+};
+
+#endif
