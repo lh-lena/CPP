1fbf1c61b2877d795578d5428e97e896534d2e26
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-07-17 17:53:48 +0200

    refactor: add function reversePolishNotationProcess

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -1,5 +1,36 @@
 #include "RPN.hpp"
 
+void    reversePolishNotationProcess(RPN *stack, const std::string &input)
+{
+    std::istringstream iss(input);
+    std::string el;
+    while (iss)
+    {
+        if (iss >> el)
+        {
+                if (stack->isOperator(el) && stack->getSize() > 1)
+                {
+                    int rightNbr = stack->getValue();
+                    stack->popValue();
+                    int leftNbr = stack->getValue();
+                    stack->popValue();
+                    int res = stack->evaluateExpression(leftNbr, rightNbr, el[0]);
+                    stack->setValue(res);
+                }
+                else if (!stack->isOperator(el))
+                {
+                    std::stringstream ss(el);
+                    int value;
+                    if (ss >> value)
+                        stack->setValue(value);
+                    else
+                        throw RPN::RpnException("Error");
+                }
+                else
+                    ;
+        }
+    }
+}
 
 int main(int ac, char **av)
 {
@@ -10,41 +41,14 @@ int main(int ac, char **av)
     if (input.empty())
         return (std::cerr << "Error: invalid arguments" << std::endl, 2);
     RPN stack;
-    std::istringstream iss(input);
-    std::string el;
-    while (iss)
+    try
     {
-        if (iss >> el)
-        {
-            try
-            {
-                if (stack.isOperator(el) && stack.getSize() > 1)
-                {
-                    int rightNbr = stack.getValue();
-                    stack.popValue();
-                    int leftNbr = stack.getValue();
-                    stack.popValue();
-                    int res = stack.evaluateExpression(leftNbr, rightNbr, el[0]);
-                    stack.setValue(res);
-                }
-                else if (!stack.isOperator(el))
-                {
-                    std::stringstream ss(el);
-                    int value;
-                    if (ss >> value)
-                        stack.setValue(value);
-                    else
-                        throw RPN::RpnException("Error");
-                }
-                else
-                    ;
-            }
-            catch(const std::exception& e)
-            {
-                std::cerr << e.what() << std::endl;
-                return (-1);
-            }
-        }
+        reversePolishNotationProcess(&stack, input);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return (-1);
     }
     std::cout <<  stack.getValue() << std::endl;
     return (0);
